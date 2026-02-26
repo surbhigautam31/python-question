32c908aaa956e2cc22348eb9e8bf35744f582b0a
surbhi gautam <surbhigauttam069@gmail.com>
2026-02-26 20:07:57 +0530

    Add files via upload

---
+++ larg.py
@@ -0,0 +1,14 @@
+#a = int(input("enter first number :"))
+#b = int(input("enter second number :"))
+#c = int(input("enter third number :"))
+
+#largest = max(a,b,c)
+#print("largest number is :",largest)
+
+a = 12
+b = 11
+c = 13
+
+
+smallest = min( a,b,c)
+print("smallest number is :",smallest)
