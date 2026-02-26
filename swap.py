c93591d4ad00353991ef4495e833ada2e66dc758
surbhi gautam <surbhigauttam069@gmail.com>
2026-02-26 20:08:25 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,11 @@
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+
+print("before swapping :")
+print("a=",a)
+print("b=",b)
+
+a,b=b,a
+print("after swapping")
+print("a=",a)
+print("b=",b)
