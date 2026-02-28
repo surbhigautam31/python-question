a06b564c5f14f6dca83ab35602f2fcafc3929ebc
surbhi gautam <surbhigauttam069@gmail.com>
2026-02-28 21:20:32 +0530

    Add files via upload

---
+++ StudentGrade.py
@@ -0,0 +1,11 @@
+marks = input("enter your marks")
+if int(marks)>=90:
+    print("you got A grade")
+elif int(marks)>=80:
+    print("you got B grade")
+elif int(marks)>=70:
+    print("you got C grade")
+elif int(marks)>=60:
+    print("you got D grade")
+else :
+    print("you got F grade")
