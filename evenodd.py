12caf2e5fc140c9af540fd654fec0ba448d69813
surbhi gautam <surbhigauttam069@gmail.com>
2026-02-28 20:30:29 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,12 @@
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+
+if a % 2 == 0 :
+ print("number is even ")
+else: 
+ print("number is odd ")
+
+if b % 2 == 0 :
+ print("number is even ")
+else: 
+ print("number is odd ")
