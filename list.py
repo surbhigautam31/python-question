26278986a409fcea8d3e4c983ed6926edddaae01
surbhi gautam <surbhigauttam069@gmail.com>
2026-03-07 15:59:34 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+mov1 = input("enter your first favourite movie :")
+mov2 = input("enter your second favourite movie :")
+mov3 = input("enter your third favourite movie :")
+
+movies = [mov1 , mov2 , mov3]
+print("yours favourite movies are :" , movies)
