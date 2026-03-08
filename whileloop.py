3c86812547c79d7b193ceff1628eeba4980a14f1
surbhi gautam <surbhigauttam069@gmail.com>
2026-03-08 15:18:58 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,14 @@
+#i = 100 
+#while i >=1 :
+    #print(i)
+    #i -=1
+    #i += 1
+    #print("done")
+
+#table of n 
+
+n = int (input("enter the number :"))
+i = 1
+while i<=10 :
+        print(n*i)
+        i = i+1
