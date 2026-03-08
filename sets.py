d73becc1c50393d3bd5a41fa8209aa19ebd0698b
surbhi gautam <surbhigauttam069@gmail.com>
2026-03-08 18:09:44 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,19 @@
+set1 = { 10, 20 , 40, 90 }
+
+# remove an element from set
+
+set1.remove(20)
+print(set1)
+
+# set2 = { 20, 40, 90 , 100}
+
+# diff btw 2 sets
+
+# result = set1.difference(set2)
+
+# print(result)
+
+# result = set1.union(set2)
+
+# result= set1.intersection(set2)
+# print(result)
